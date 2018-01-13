c957ba6230ed2b54b106f1b8eadf17503fb432c9
WenHaiGo <2499039129@qq.com>
2018-01-13 20:44:23 +0800

    为了使用分页功能，添加了俩个方法获取数据库表的总条数 得到具体某一页一共多少数据

--- src/cn/me/dao/BookDao.java
+++ src/cn/me/dao/BookDao.java
@@ -5,9 +5,47 @@ import java.util.List;
 import cn.me.model.Book;
 
 public interface BookDao {
+	/**
+	 * 得到所有书籍
+	 * 
+	 * @return
+	 */
 	List<Book> getAllBook();
+
 	int delBookById(int bid);
-	Book findById( int bid);
+
+	/**
+	 * 通过id查找图书信息
+	 * 
+	 * @param bid
+	 * @return
+	 */
+	Book findById(int bid);
+
+	/**
+	 * 修改图书信息
+	 * 
+	 * @param book
+	 * @return
+	 */
 	int update(Book book);
+
+	/**
+	 * 添加图书
+	 * 
+	 * @param book
+	 * @return
+	 */
 	int add(Book book);
+
+	/**
+	 * 
+	 * 得到总的数据条数 但是请注意如何判断是得到用户的总条数还是图书的总条数？？
+	 * 
+	 * @return
+	 */
+	int getTotalNum();
+	
+	<T> List<T> getPageList(int pageStart,int pageSize);
+
 }
