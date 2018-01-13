bc2925ee428495126121b55562a9ce72e631f086
WenHaiGo <2499039129@qq.com>
2018-01-13 20:46:25 +0800

    为了使用分页特地用了一个分页的工具类

---
+++ src/cn/me/dbutil/PageUtil.java
@@ -0,0 +1,44 @@
+package cn.me.dbutil;
+
+import java.util.List;
+
+//处理分页的工具类
+public class PageUtil<T> {
+	int pageNo;
+	int pageSize;
+	int totalPage;
+	List<T> list;
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public int getTotalPage() {
+		return totalPage;
+	}
+
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+
+	public List<T> getList() {
+		return list;
+	}
+
+	public void setList(List<T> list) {
+		this.list = list;
+	}
+
+}
